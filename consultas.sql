8fa4ab024c84683fc7c98e6fab192fbf18b1934e
Raul Vilar <raul.s.vilar@gmail.com>
2018-09-03 20:22:29 -0300

    Update consultas.sql
    Identação

--- consultas.sql
+++ consultas.sql
@@ -1,38 +1,170 @@
+--Lista as informações dos clientes maiores de 20 anos
+
+SELECT 
+    *
+FROM
+    hotel.cliente c
+WHERE
+    c.idade > 20
+
 /*Listando funcionarios, com salarios maiores que 1600, por nome, cpf, salario e funcao*/
-select p.p_nome, p.sobrenome, p.cpf, f.salario, r.funcao from
-hotel.funcionario f join hotel.pessoa p on(f.pessoa_cpf=p.cpf)
-natural join hotel.responsavel r where (salario>1600);
+
+SELECT
+    p.p_nome, p.sobrenome, p.cpf, f.salario, r.funcao
+FROM
+    hotel.funcionario f
+    JOIN hotel.pessoa p
+        ON(f.pessoa_cpf=p.cpf)
+    NATURAL JOIN
+        hotel.responsavel r
+WHERE (salario>1600);
 
 /*Listando clientes com reservas por nome, telefone, email e tipo de quarto*/
-select p.p_nome, p.sobrenome, t.telefone, e.email, sq.tipo_quarto
-from hotel.pessoa p join hotel.email e on(pessoa_cpf=cpf)
-join hotel.telefone t using(pessoa_cpf) join
-(select c.cod_cliente, pessoa_cpf, r.tipo_quarto from hotel.cliente c 
-join hotel.reserva r using(cod_cliente)) as sq using (pessoa_cpf);
+SELECT
+    p.p_nome, p.sobrenome, t.telefone, e.email, sq.tipo_quarto
+FROM
+    hotel.pessoa p
+    JOIN 
+        hotel.email e 
+        ON(pessoa_cpf=cpf)
+    JOIN
+        hotel.telefone t
+        USING(pessoa_cpf)
+    JOIN
+        (SELECT
+            c.cod_cliente, pessoa_cpf, r.tipo_quarto
+        FROM hotel.cliente c 
+            JOIN hotel.reserva r
+                USING(cod_cliente))  AS sq 
+        USING (pessoa_cpf);
 
-/*Listando as informações do cliente mais velho*/
-select * from hotel.pessoa p where p.data_nascimento = 
-(select min(data_nascimento) from hotel.pessoa p1 
-join hotel.cliente c on(p1.cpf=c.pessoa_cpf));
+/*Listando  AS informações do cliente mais velho*/
+SELECT
+    *
+FROM
+    hotel.pessoa p 
+WHERE
+    p.data_nascimento = (
+                        SELECT 
+                            min(data_nascimento)
+                        FROM
+                            hotel.pessoa p1 
+                            JOIN hotel.cliente c
+                                ON(p1.cpf=c.pessoa_cpf));
 
 /*Listando nome e email das pessoas com email do dcomp*/
-select p_nome, sobrenome, email from hotel.pessoa p1 
-join hotel.email e on(p1.cpf=e.pessoa_cpf) 
-where email like '%dcomp.ufs.br';
+SELECT
+    p_nome, sobrenome, email
+FROM
+    hotel.pessoa p1 
+    JOIN 
+        hotel.email e
+        ON(p1.cpf=e.pessoa_cpf) 
+WHERE
+    email like '%dcomp.ufs.br';
 
 /*Listando nome dos dependentes e seus responsaveis*/
-select d.p_nome, d.sobrenome, cod_responsavel, sq.p_nome, sq.sobrenome
-from hotel.dependentes d join (select p_nome, cod_cliente, sobrenome
-from hotel.pessoa p join hotel.cliente c on(p.cpf=c.pessoa_cpf))
-as sq on(sq.cod_cliente=d.cod_responsavel) order by d.sobrenome;
+SELECT
+    d.p_nome, d.sobrenome, cod_responsavel, sq.p_nome, sq.sobrenome
+FROM
+    hotel.dependentes d
+    JOIN (
+        SELECT
+            p_nome, cod_cliente, sobrenome
+        FROM
+            hotel.pessoa p
+            JOIN hotel.cliente c
+            ON(p.cpf=c.pessoa_cpf))
+        as sq ON(sq.cod_cliente=d.cod_responsavel)
+ORDER BY d.sobrenome;
 
 /*Listando clientes em ordem de maior gasto em itens*/
-select p_nome, sobrenome, sq.num_registro, sq.s from hotel.pessoa 
-join hotel.cliente on (cpf=pessoa_cpf) join hotel.registro using(cod_cliente)
-natural join (select a.num_registro, sum(i.valor*a.quantidade) s from hotel.itens i
-join hotel.aloca a using(cod_item) group by a.num_registro) as sq order by s desc;
+SELECT
+    p_nome, sobrenome, sq.num_registro, sq.s
+FROM
+    hotel.pessoa 
+    JOIN
+        hotel.cliente
+        ON (cpf=pessoa_cpf)
+        JOIN
+            hotel.registro
+            USING(cod_cliente)
+            NATURAL JOIN 
+                (SELECT
+                    a.num_registro, sum(i.valor*a.quantidade) s
+                FROM
+                    hotel.itens i
+                    JOIN
+                        hotel.aloca a
+                        USING(cod_item)
+                GROUP BY
+                    a.num_registro) AS sq
+ORDER BY s desc;
+
+--Ta parecido com esse de cima, mas são os clientes que compraram mais que a média
+
+SELECT
+	p_nome, sobrenome, num_registro, sum(i.valor*a.quantidade) valor_items
+FROM
+    hotel.pessoa 
+    JOIN
+        hotel.cliente
+        ON (cpf=pessoa_cpf)
+        JOIN
+            hotel.registro
+            USING(cod_cliente)
+            JOIN 
+				hotel.aloca a
+				USING(num_registro)
+				JOIN
+					hotel.itens i
+                    USING(cod_item)
+GROUP BY
+	num_registro,
+	pessoa.p_nome,
+	sobrenome
+HAVING
+	sum(i.valor*a.quantidade) > (
+					SELECT
+						AVG(tb1.valor_items)
+					FROM
+						(SELECT
+							p_nome, sobrenome, num_registro, sum(i.valor*a.quantidade) as valor_items
+						FROM
+							hotel.pessoa 
+							JOIN
+								hotel.cliente
+								ON (cpf=pessoa_cpf)
+								JOIN
+									hotel.registro
+									USING(cod_cliente)
+									JOIN 
+										hotel.aloca a
+										USING(num_registro)
+										JOIN
+											hotel.itens i
+											USING(cod_item)
+						GROUP BY
+							num_registro,
+							pessoa.p_nome,
+							sobrenome) as tb1)
 
 /*Lista o total em R$ de entrada*/
-select (sum(i.valor*a.quantidade) + sum(t.valor)) as entradas from hotel.aloca a natural join
-hotel.itens i join hotel.registro r using(num_registro) join hotel.ocupa o using(num_registro) join
-hotel.quarto q on(o.num_quarto=q.numero) join hotel.tipo t on(q.tipo=t.nome);
+SELECT
+    (sum(i.valor*a.quantidade) + sum(t.valor)) AS entradas
+FROM
+    hotel.aloca a
+    NATURAL JOIN
+        hotel.itens i
+        JOIN
+          hotel.registro r 
+          USING(num_registro)
+          JOIN
+            hotel.ocupa o 
+            USING(num_registro)
+            JOIN
+                hotel.quarto q
+                ON (o.num_quarto=q.numero)
+                JOIN
+                  hotel.tipo t
+                  ON (q.tipo=t.nome);
